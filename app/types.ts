98dba6c59bf36a67ead19739fd3efd61bd721dfe
Rodrigo <rod_novaes@hotmail.com>
2025-10-13 19:57:28 +0200

    Update Project types

--- app/types.ts
+++ app/types.ts
@@ -1,9 +1,11 @@
 export type Project = {
   id: string;
+  documentId: string;
   title: string;
   description: string;
   image: string;
   url: string;
-  data: string;
+  date: string;
+  category: string;
   featured: boolean;
 };
